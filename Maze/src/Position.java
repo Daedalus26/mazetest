93ade73124fe9bc35a6e12ac07d9fceb76dfcad5
Daedalus26 <Daedalus26>
2020-05-02 19:11:17 +0100

    basics of a maze

---
+++ Maze/src/Position.java
@@ -0,0 +1,12 @@
+
+public class Position {
+	public int x;
+	//x = column
+	public int y;
+	//y = row
+	
+	public Position(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
--- Maze/src/newMaze.java
+++ Maze/src/newMaze.java
@@ -1,9 +1,20 @@
+import java.util.LinkedList;
 
 public class newMaze {
-
+	static int[][] maze = {
+			{2, 0, 1, 1},
+			{1, 1, 0, 1},
+			{0, 1, 1, 1}
+	};
+	
+	static LinkedList<Position> path = new LinkedList<Position>();
 	public static void main(String[] args) {
-
-
+		
+		Position p = new Position(3, 0);
+		path.push(p);
+		maze[path.peek().y][path.peek().x] = 0;
+		
+		
 	}
 
 }
