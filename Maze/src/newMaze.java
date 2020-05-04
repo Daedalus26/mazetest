1ec26b84eeb1adfdac265638aca3ee74b1c8e5c4
Daedalus26 <Daedalus26>
2020-05-04 03:13:36 +0100

    new maze

--- Maze/src/newMaze.java
+++ Maze/src/newMaze.java
@@ -2,9 +2,13 @@ import java.util.LinkedList;
 
 public class newMaze {
 	static int[][] maze = {
-			{2, 0, 1, 1},
-			{1, 1, 0, 1},
-			{0, 1, 1, 1}
+			{2, 1, 1, 1, 1, 1, 1, 1},
+			{1, 0, 0, 1, 0, 1, 0, 1},
+			{1, 0, 0, 1, 0, 1, 0, 1},
+			{1, 0, 1, 1, 1, 0, 0, 1},
+			{0, 1, 0, 0, 1, 1, 0, 0},
+			{0, 1, 1, 1, 1, 1, 1, 1}
+
 	};
 	
 	static LinkedList<Position> path = new LinkedList<Position>();
@@ -19,6 +23,7 @@ public class newMaze {
 			
 			maze[y][x] = 0;
 		//down
+			if(isValid(y+1, x)) {
 		if(maze[y+1][x] == 2) {
 			System.out.println("Moved down. you won!");
 			return;
@@ -28,7 +33,10 @@ public class newMaze {
 				System.out.println("Moved down");
 				continue;
 				}
+			}
+		
 				//left
+				if(isValid(y, x-1)) {
 				if(maze[y][x-1] == 2) {
 					System.out.println("Moved left. you won!");
 					return;
@@ -38,7 +46,10 @@ public class newMaze {
 						System.out.println("Moved left");
 						continue;
 						}
+				}
+				
 				//up
+				if(isValid(y-1, x)) {
 				if(maze[y-1][x] == 2) {
 					System.out.println("Moved up. you won!");
 					return;
@@ -48,7 +59,10 @@ public class newMaze {
 						System.out.println("Moved up");
 						continue;
 						}
+				}
+				
 				//down
+				if(isValid(y+1, x)) {
 				if(maze[y+1][x] == 2) {
 					System.out.println("Moved down. you won!");
 					return;
@@ -58,7 +72,10 @@ public class newMaze {
 						System.out.println("Moved down");
 						continue;
 						}
+				}
+				
 				//right
+				if(isValid(y+1, x)) {
 				if(maze[y+1][x] == 2) {
 					System.out.println("Moved right. you won!");
 					return;
@@ -68,15 +85,30 @@ public class newMaze {
 						System.out.println("Moved down");
 						continue;
 						}
+					}
 				path.pop();
+				System.out.println("Moved back");
+				
 				if(path.size() <= 0) {
 					System.out.println("no path");
+					return;
 				}
-			
+	
 			}
+			
 		}
 		
-		
+		public static boolean isValid(int y, int x) { 
+			if(y < 0 ||
+				y >= maze.length ||
+				x < 0 ||
+				x >= maze[y].length
+					) {
+			
+			return false;
+			}
+			return true;
+		}
 	}
 
 
