8e3f4be868f28768bc0ee5b3840904289582d39b
Daedalus26 <Daedalus26>
2020-05-04 03:51:38 +0100

    clean maze

--- Maze/src/newMaze.java
+++ Maze/src/newMaze.java
@@ -13,91 +13,99 @@ public class newMaze {
 	
 	static LinkedList<Position> path = new LinkedList<Position>();
 	public static void main(String[] args) {
-		
-		Position p = new Position(0, 3);
-		path.push(p);
-		
-		while(true) {
-			int y = path.peek().y;
-			int x = path.peek().x;
 			
-			maze[y][x] = 0;
-		//down
-			if(isValid(y+1, x)) {
-		if(maze[y+1][x] == 2) {
-			System.out.println("Moved down. you won!");
-			return;
-					
-		}else if(maze[y+1][x] == 1) {
-				path.push(new Position(y+1, x));
-				System.out.println("Moved down");
-				continue;
-				}
-			}
-		
-				//left
-				if(isValid(y, x-1)) {
-				if(maze[y][x-1] == 2) {
-					System.out.println("Moved left. you won!");
-					return;
-							
-				}else if(maze[y][x-1] == 1) {
-						path.push(new Position(y, x-1));
-						System.out.println("Moved left");
-						continue;
-						}
-				}
-				
-				//up
-				if(isValid(y-1, x)) {
-				if(maze[y-1][x] == 2) {
-					System.out.println("Moved up. you won!");
-					return;
-							
-				}else if(maze[y-1][x] == 1) {
-						path.push(new Position(y-1, x));
-						System.out.println("Moved up");
-						continue;
-						}
-				}
-				
-				//down
-				if(isValid(y+1, x)) {
-				if(maze[y+1][x] == 2) {
-					System.out.println("Moved down. you won!");
-					return;
-							
-				}else if(maze[y][x-1] == 1) {
-						path.push(new Position(y+1, x));
-						System.out.println("Moved down");
-						continue;
-						}
-				}
-				
-				//right
-				if(isValid(y+1, x)) {
-				if(maze[y+1][x] == 2) {
-					System.out.println("Moved right. you won!");
-					return;
-							
-				}else if(maze[y+1][x] == 1) {
-						path.push(new Position(y+1, x));
-						System.out.println("Moved down");
-						continue;
-						}
-					}
-				path.pop();
-				System.out.println("Moved back");
-				
-				if(path.size() <= 0) {
-					System.out.println("no path");
-					return;
-				}
-	
-			}
-			
+		if(solveMaze(new Position(0, 3))) {
+		System.out.println("You left the maze");
+		}else {
+			System.out.println("No Path");
 		}
 		
+	}
+		private static boolean solveMaze(Position p) {
+			path.push(p);
+			
+			while(true) {
+				int y = path.peek().y;
+				int x = path.peek().x;
+				
+				maze[y][x] = 0;
+			//down
+				if(isValid(y+1, x)) {
+			if(maze[y+1][x] == 2) {
+				System.out.println("Moved down");
+				return true;
+						
+			}else if(maze[y+1][x] == 1) {
+					path.push(new Position(y+1, x));
+					System.out.println("Moved down");
+					continue;
+					}
+				}
+			
+					//left
+					if(isValid(y, x-1)) {
+					if(maze[y][x-1] == 2) {
+						System.out.println("Moved left");
+						return true;
+								
+					}else if(maze[y][x-1] == 1) {
+							path.push(new Position(y, x-1));
+							System.out.println("Moved left");
+							continue;
+							}
+					}
+					
+					//up
+					if(isValid(y-1, x)) {
+					if(maze[y-1][x] == 2) {
+						System.out.println("Moved up");
+						return true;
+								
+					}else if(maze[y-1][x] == 1) {
+							path.push(new Position(y-1, x));
+							System.out.println("Moved up");
+							continue;
+							}
+					}
+					
+					//down
+					if(isValid(y+1, x)) {
+					if(maze[y+1][x] == 2) {
+						System.out.println("Moved down");
+						return true;
+								
+					}else if(maze[y][x-1] == 1) {
+							path.push(new Position(y+1, x));
+							System.out.println("Moved down");
+							continue;
+							}
+					}
+					
+					//right
+					if(isValid(y+1, x)) {
+					if(maze[y+1][x] == 2) {
+						System.out.println("Moved right");
+						return true;
+								
+					}else if(maze[y+1][x] == 1) {
+							path.push(new Position(y+1, x));
+							System.out.println("Moved down");
+							continue;
+							}
+						}
+					path.pop();
+					System.out.println("Moved back");
+					
+					if(path.size() <= 0) {
+						return false;
+					}
+		
+				}
+				
+			}
+		
+	
+
 		public static boolean isValid(int y, int x) { 
 			if(y < 0 ||
 				y >= maze.length ||
@@ -107,6 +115,7 @@ public class newMaze {
 			
 			return false;
 			}
+		
 			return true;
 		}
 	}
